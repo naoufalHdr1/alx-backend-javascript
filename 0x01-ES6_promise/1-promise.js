24d0c190e4a8f4e7121988beced2ac61879d4754
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-02 20:37:47 +0100

    Task 1: fix requirement check error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success)
-      resolve({ status: 200, body: 'Success', });
-    else
+    if (success) {
+      resolve({
+	status: 200,
+	body: 'Success',
+      });
+    } else {
       reject(new Error("The fake API is not working currently"));
+    }
   });
 }
