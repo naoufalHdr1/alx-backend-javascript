0794ff915b98ab8168973b00c3412dc49114a834
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-02 21:16:33 +0100

    Task 3: fixing requirement check errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,8 @@ import { uploadPhoto, createUser } from "./utils";
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }  
