470c7b7a13f388d0be798922add7f42ca68b07bf
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-03 23:05:51 +0100

    Task 10: Add Car class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Method to clone the car
+  cloneCar() {
+    return new this.constructor();
+  }
+}
+
+export default Car;
