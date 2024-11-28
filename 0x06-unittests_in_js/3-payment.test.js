432044464bed5f1bdb70f10ce9c648b3deb74b18
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-29 00:56:59 +0100

    Task 3: Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+// 3-payment.test.js 
+
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber correctly', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    spy.restore();
+  });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+// utils.js
+
+const Utils = {
+  calculateNumber (type, a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+
+    if (type === 'SUM') {
+      return roundedA + roundedB;
+    } else if (type === 'SUBTRACT') {
+      return roundedA - roundedB;
+    } else if (type === 'DIVIDE') {
+      if (b == 0)
+        return 'Error';
+      return roundedA / roundedB;
+    }
+
+    throw new Error('Invalid operation type');
+  }
+};
+
+module.exports = Utils;
