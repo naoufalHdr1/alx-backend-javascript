6d956ab2df20e1db5eae087a137c9597dea2d3f9
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-29 01:53:15 +0100

    Task 4: stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+// 3-payment.test.js
+
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should stub Utils.calculateNumber and verify behavior', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    stub.restore();
+    consoleSpy.restore();
+  });
+});
