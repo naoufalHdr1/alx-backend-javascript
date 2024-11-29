4b9f9c5d3ff2d7ce42a18ec81d9ab71f4ba20c16
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-29 20:33:18 +0100

    Task 5: Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,32 @@
+// 3-payment.test.js
+
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log the correct total for inputs 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('should log the correct total for inputs 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
