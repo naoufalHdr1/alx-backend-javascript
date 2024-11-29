36328b9f922a8e9f1a610f0a54db134c3e8f9e90
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-29 21:38:32 +0100

    Task 6: fix check 3 error

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,10 +3,10 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
 const { expect } = require('chai');
 
-describe('Integration with external API', () => {
-  it('should return a resolved promise with data when success is true', () => {
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with data when success is true', (done) => {
     getPaymentTokenFromAPI(true).then((response) => {
-      expect(response).to.be.equal({ data: 'Successful response from the API' });
+      expect(response).to.deep.equal({ data: 'Successful response from the API' });
       done();
     });
   });
